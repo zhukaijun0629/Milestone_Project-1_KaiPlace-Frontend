32af7fca7d40a86b9f11630fded37288203135fd
zhukaijun0629 <kaijun.zhu0629@gmail.com>
2021-02-17 09:01:26 -0500

    revised user list order

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React,{ useContext } from 'react';
 
 import UserItem from './UserItem';
 import Card from '../../shared/components/UIElements/Card';
 import './UsersList.css';
+import { AuthContext } from "../../shared/context/auth-context";
+
 
 const UsersList = props => {
+  const auth = useContext(AuthContext);
+
   if (props.items.length === 0) {
     return (
       <div className="center">
@@ -15,9 +19,20 @@ const UsersList = props => {
     );
   }
 
+  const loggedInUser = props.items.find(user => user.id === auth.userId)
+
   return (
     <ul className="users-list">
-      {props.items.map(user => (
+      {loggedInUser && (
+        <UserItem
+          key={loggedInUser.id}
+          id={loggedInUser.id}
+          image={loggedInUser.image}
+          name={loggedInUser.name}
+          placeCount={loggedInUser.places.length}
+        />
+      )}
+      {props.items.map(user => user.id !== auth.userId && (
         <UserItem
           key={user.id}
           id={user.id}
